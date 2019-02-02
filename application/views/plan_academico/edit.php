2c361815b8a2974963a66b92c758c4620124cb7f
Mario escobar <escobar_m_98@hotmail.com>
2019-02-02 13:18:20 -0400

    plan academico falta..

--- application/views/plan_academico/edit.php
+++ application/views/plan_academico/edit.php
@@ -2,28 +2,31 @@
     <div class="col-md-12">
       	<div class="box box-info">
             <div class="box-header with-border">
-              	<h3 class="box-title">Plan Academico Edit</h3>
+              	<h3 class="box-title">Editar Plan Académico</h3>
             </div>
-			<?php echo form_open('plan_academico/edit/'.$plan_academico['plan_academico_id']); ?>
+			<?php echo form_open('plan_academico/edit/'.$planacad['planacad_id']); ?>
 			<div class="box-body">
 				<div class="row clearfix">
 					<div class="col-md-6">
-						<label for="estado_id" class="control-label">Estado</label>
+						<label for="planacad_nombre" class="control-label"><span class="text-danger">*</span>Nombre</label>
 						<div class="form-group">
-							<select name="estado_id" class="form-control">
-								<option value="">select estado</option>
-								<?php 
-								foreach($all_estado as $estado)
-								{
-									$selected = ($estado['estado_id'] == $plan_academico['estado_id']) ? ' selected="selected"' : "";
-
-									echo '<option value="'.$estado['estado_id'].'" '.$selected.'>'.$estado['estado_descripcion'].'</option>';
-								} 
-								?>
-							</select>
+							<input type="text" name="planacad_nombre" value="<?php echo ($this->input->post('planacad_nombre') ? $this->input->post('planacad_nombre') : $planacad['planacad_nombre']); ?>" class="form-control" id="planacad_nombre" />
+							<span class="text-danger"><?php echo form_error('planacad_nombre');?></span>
 						</div>
 					</div>
+					<!--<div class="col-md-6">
+						<label for="planacad_feccreacion" class="control-label">Plan Academico Feccreacion</label>
+						<div class="form-group">
+							<input type="text" name="planacad_feccreacion" value="<?php //echo ($this->input->post('planacad_feccreacion') ? $this->input->post('planacad_feccreacion') : $planacad['planacad_feccreacion']); ?>" class="has-datepicker form-control" id="planacad_feccreacion" />
+						</div>
+					</div>-->
 					<div class="col-md-6">
+						<label for="planacad_codigo" class="control-label">Código</label>
+						<div class="form-group">
+							<input type="text" name="planacad_codigo" value="<?php echo ($this->input->post('planacad_codigo') ? $this->input->post('planacad_codigo') : $planacad['planacad_codigo']); ?>" class="form-control" id="planacad_codigo" />
+						</div>
+					</div>
+                                        <div class="col-md-6">
 						<label for="carrera_id" class="control-label">Carrera</label>
 						<div class="form-group">
 							<select name="carrera_id" class="form-control">
@@ -31,7 +34,7 @@
 								<?php 
 								foreach($all_carrera as $carrera)
 								{
-									$selected = ($carrera['carrera_id'] == $plan_academico['carrera_id']) ? ' selected="selected"' : "";
+									$selected = ($carrera['carrera_id'] == $planacad['carrera_id']) ? ' selected="selected"' : "";
 
 									echo '<option value="'.$carrera['carrera_id'].'" '.$selected.'>'.$carrera['carrera_nombre'].'</option>';
 								} 
@@ -40,34 +43,31 @@
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="plan_academico_nombre" class="control-label"><span class="text-danger">*</span>Plan Academico Nombre</label>
+						<label for="planacad_titmodalidad" class="control-label">Tit. Modalidad</label>
 						<div class="form-group">
-							<input type="text" name="plan_academico_nombre" value="<?php echo ($this->input->post('plan_academico_nombre') ? $this->input->post('plan_academico_nombre') : $plan_academico['plan_academico_nombre']); ?>" class="form-control" id="plan_academico_nombre" />
-							<span class="text-danger"><?php echo form_error('plan_academico_nombre');?></span>
+							<input type="text" name="planacad_titmodalidad" value="<?php echo ($this->input->post('planacad_titmodalidad') ? $this->input->post('planacad_titmodalidad') : $planacad['planacad_titmodalidad']); ?>" class="form-control" id="planacad_titmodalidad" />
 						</div>
 					</div>
 					<div class="col-md-6">
-						<label for="plan_academico_feccreacion" class="control-label">Plan Academico Feccreacion</label>
+						<label for="planacad_cantgestion" class="control-label">Cant. Gestion</label>
 						<div class="form-group">
-							<input type="text" name="plan_academico_feccreacion" value="<?php echo ($this->input->post('plan_academico_feccreacion') ? $this->input->post('plan_academico_feccreacion') : $plan_academico['plan_academico_feccreacion']); ?>" class="has-datepicker form-control" id="plan_academico_feccreacion" />
+							<input type="text" name="planacad_cantgestion" value="<?php echo ($this->input->post('planacad_cantgestion') ? $this->input->post('planacad_cantgestion') : $planacad['planacad_cantgestion']); ?>" class="form-control" id="planacad_cantgestion" />
 						</div>
 					</div>
-					<div class="col-md-6">
-						<label for="plan_academico_codigo" class="control-label">Plan Academico Codigo</label>
+                                        <div class="col-md-6">
+						<label for="estado_id" class="control-label">Estado</label>
 						<div class="form-group">
-							<input type="text" name="plan_academico_codigo" value="<?php echo ($this->input->post('plan_academico_codigo') ? $this->input->post('plan_academico_codigo') : $plan_academico['plan_academico_codigo']); ?>" class="form-control" id="plan_academico_codigo" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="plan_academico_titmodalidad" class="control-label">Plan Academico Titmodalidad</label>
-						<div class="form-group">
-							<input type="text" name="plan_academico_titmodalidad" value="<?php echo ($this->input->post('plan_academico_titmodalidad') ? $this->input->post('plan_academico_titmodalidad') : $plan_academico['plan_academico_titmodalidad']); ?>" class="form-control" id="plan_academico_titmodalidad" />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<label for="plan_academico_cantgestion" class="control-label">Plan Academico Cantgestion</label>
-						<div class="form-group">
-							<input type="text" name="plan_academico_cantgestion" value="<?php echo ($this->input->post('plan_academico_cantgestion') ? $this->input->post('plan_academico_cantgestion') : $plan_academico['plan_academico_cantgestion']); ?>" class="form-control" id="plan_academico_cantgestion" />
+							<select name="estado_id" class="form-control">
+								<option value="">select estado</option>
+								<?php 
+								foreach($all_estado as $estado)
+								{
+									$selected = ($estado['estado_id'] == $planacad['estado_id']) ? ' selected="selected"' : "";
+
+									echo '<option value="'.$estado['estado_id'].'" '.$selected.'>'.$estado['estado_descripcion'].'</option>';
+								} 
+								?>
+							</select>
 						</div>
 					</div>
 				</div>
--- application/views/plan_academico/index.php
+++ application/views/plan_academico/index.php
@@ -1,3 +1,6 @@
+<!------------------ ESTILO DE LAS TABLAS ----------------->
+<link href="<?php echo base_url('resources/css/mitabla.css'); ?>" rel="stylesheet">
+<!-------------------------------------------------------->
 <div class="box-header">
                 <h3 class="box-title">Plan Academico</h3>
             	<div class="box-tools">
@@ -9,31 +12,31 @@
         <div class="box">
             
             <div class="box-body">
-                <table class="table table-striped">
+                <table class="table table-striped" id="mitabla">
                     <tr>
 						<th>#</th>
 						<th>Nombre</th>
-						<th>Carrera Id</th>
-						<th>Fecha Creación</th>
-						<th>Plan Academico Codigo</th>
-						<th>Plan Academico Titmodalidad</th>
-						<th>Plan Academico Cantgestion</th>
-						<th>Estado Id</th>
-						<th>Actions</th>
+						<!--<th>Fecha Creación</th>-->
+						<th>Codigo</th>
+						<th>Carrera</th>
+						<th>Titulo/Modalidad</th>
+						<th>Cant. Gestion</th>
+						<th>Estado</th>
+						<th></th>
                     </tr>
                     <?php foreach($plan_academico as $p){ ?>
                     <tr>
-						<td><?php echo $p['plan_academico_id']; ?></td>
-						<td><?php echo $p['estado_id']; ?></td>
+						<td><?php echo $p['planacad_id']; ?></td>
+						<td><?php echo $p['planacad_nombre']; ?></td>
+						<!--<td><?php //echo $p['planacad_feccreacion']; ?></td>-->
+						<td><?php echo $p['planacad_codigo']; ?></td>
 						<td><?php echo $p['carrera_id']; ?></td>
-						<td><?php echo $p['plan_academico_nombre']; ?></td>
-						<td><?php echo $p['plan_academico_feccreacion']; ?></td>
-						<td><?php echo $p['plan_academico_codigo']; ?></td>
-						<td><?php echo $p['plan_academico_titmodalidad']; ?></td>
-						<td><?php echo $p['plan_academico_cantgestion']; ?></td>
+						<td><?php echo $p['planacad_titmodalidad']; ?></td>
+						<td><?php echo $p['planacad_cantgestion']; ?></td>
+						<td><?php echo $p['estado_id']; ?></td>
 						<td>
-                            <a href="<?php echo site_url('plan_academico/edit/'.$p['plan_academico_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
-                            <a href="<?php echo site_url('plan_academico/remove/'.$p['plan_academico_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
+                            <a href="<?php echo site_url('plan_academico/edit/'.$p['planacad_id']); ?>" class="btn btn-info btn-xs"><span class="fa fa-pencil"></span> Edit</a> 
+                            <a href="<?php echo site_url('plan_academico/remove/'.$p['planacad_id']); ?>" class="btn btn-danger btn-xs"><span class="fa fa-trash"></span> Delete</a>
                         </td>
                     </tr>
                     <?php } ?>
