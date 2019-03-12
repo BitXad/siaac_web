f755223b6eaaacad002ac6299982e2e5d24e0210
alejandro <ale.hv10@gmail.com>
2019-03-12 17:06:25 -0400

    aula

--- application/controllers/Aula.php
+++ application/controllers/Aula.php
@@ -53,53 +53,6 @@ class Aula extends CI_Controller{
         }
     }  
 
-    public function generar_excel(){
-    $llamadas = $this->Aula_model->get_all_aula();
-    if(count($llamadas) > 0){
-        //Cargamos la librería de excel.
-        $this->load->library('excel'); $this->excel->setActiveSheetIndex(0);
-        $this->excel->getActiveSheet()->setTitle('Llamadas');
-        //Contador de filas
-        $contador = 1;
-        //Le aplicamos ancho las columnas(OPCIONAL).
-        $this->excel->getActiveSheet()->getColumnDimension('A')->setWidth(30);
-        $this->excel->getActiveSheet()->getColumnDimension('B')->setWidth(30);
-        $this->excel->getActiveSheet()->getColumnDimension('C')->setWidth(30);
-        //Le aplicamos negrita a los títulos de la cabecera(OPCIONAL).
-        $this->excel->getActiveSheet()->getStyle("A{$contador}")->getFont()->setBold(true);
-        $this->excel->getActiveSheet()->getStyle("B{$contador}")->getFont()->setBold(true);
-        $this->excel->getActiveSheet()->getStyle("C{$contador}")->getFont()->setBold(true);
-        //Definimos los títulos de la cabecera.
-        $this->excel->getActiveSheet()->setCellValue("A{$contador}", 'Nombre');
-        $this->excel->getActiveSheet()->setCellValue("B{$contador}", 'Descripcion');
-        $this->excel->getActiveSheet()->setCellValue("C{$contador}", 'Capacidad');
-        $this->excel->getActiveSheet()->setCellValue("D{$contador}", 'Tipo');
-        //Definimos la data del cuerpo.        
-        foreach($llamadas as $l){
-           //Incrementamos una fila más, para ir a la siguiente.
-           $contador++;
-           //Informacion de las filas de la consulta.
-         
-           $this->excel->getActiveSheet()->setCellValue("A{$contador}", $l['aula_nombre']);
-           $this->excel->getActiveSheet()->setCellValue("B{$contador}", $l['aula_descripcion']);
-           $this->excel->getActiveSheet()->setCellValue("C{$contador}", $l['aula_capacidad']);
-           $this->excel->getActiveSheet()->setCellValue("D{$contador}", $l['tipoaula_descripcion']);
-        }
-        //Le ponemos un nombre al archivo que se va a generar.
-        $hoy = date('d/m/Y H:i:s');
-        $archivo = "Aula".$hoy.".xls";
-        header('Content-Type: application/vnd.ms-excel');
-        header('Content-Disposition: attachment;filename="'.$archivo.'"');
-        header('Cache-Control: max-age=0');
-        $objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');
-        //Hacemos una salida al navegador con el archivo Excel.
-        $objWriter->save('php://output');
-     }else{
-        echo 'No se han encontrado llamadas';
-        exit;        
-     }
-  }
-
     /*
      * Editing a aula
      */
